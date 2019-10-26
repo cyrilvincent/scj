5925993590777d2c54239e94f7a782dfa0882c9d
cyrilvincent <contact@cyrilvincent.com>
2019-10-26 19:16:59 +0200

    Refactoring

--- nasnettest.py
+++ nasnettest.py
@@ -2,9 +2,16 @@ import keras
 
 # model = keras.applications.NASNetLarge()
 # model.summary()
+# print(len(model.layers)) #1040
 #
 # model = keras.applications.NASNetMobile()
 # model.summary()
+# print(len(model.layers)) #771
 
-model = keras.applications.MobileNetV2()
+# model = keras.applications.MobileNetV2()
+# model.summary()
+# print(len(model.layers)) #157
+
+model = keras.applications.InceptionResNetV2()
 model.summary()
+print(len(model.layers)) #782
