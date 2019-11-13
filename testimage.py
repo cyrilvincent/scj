2ded4c40ac5d47a9737bc3ea2c9ac12d9f190201
cyrilvincent <contact@cyrilvincent.com>
2019-11-13 21:45:13 +0100

    WF04

--- testimage.py
+++ testimage.py
@@ -1,13 +1,101 @@
-from PIL import Image
+from PIL import Image, ImageStat, ImageEnhance
+import numpy as np
 
 radius = 875
 
 path = "tests/img/OSD185236-Analysees-01-01.jpg"
+path = "tests/img/OSD185236-Brutes-01-01.bmp"
 im = Image.open(path)
-im.show()
+#im.show()
 
 im =  im.crop(((im.size[0] / 2) - radius, (im.size[1] / 2) - radius, (im.size[0] / 2) + radius, (im.size[1] / 2) + radius))
-im.show()
+#im.show()
 
-lum = 132.05
-std = 11.65
+stat = ImageStat.Stat(im)
+lum = stat.mean[0]
+std = stat.stddev[0]
+min2 = stat.extrema[0][0]
+max2 = stat.extrema[0][1]
+print(lum, std, min2, max2) #140.8660502857143 10.847677919700216 104 208
+
+norm = im.point(lambda x : np.clip(x - lum + 127, 0, 255))
+stat = ImageStat.Stat(norm)
+lum = stat.mean[0]
+std = stat.stddev[0]
+min2 = stat.extrema[0][0]
+max2 = stat.extrema[0][1]
+pxs = list(norm.getdata())
+nb0px = len([p for p in pxs if p == 0])
+print(lum, std, min2, max2, nb0px)
+#norm.show()
+
+# for c in range(1,15):
+#     ie = ImageEnhance.Contrast(norm)
+#     c1 = ie.enhance(c)
+#     stat = ImageStat.Stat(c1)
+#     lum = stat.mean[0]
+#     std = stat.stddev[0]
+#     min = stat.extrema[0][0]
+#     max = stat.extrema[0][1]
+#     pxs = list(c1.getdata())
+#     nb0px = len([p for p in pxs if p == 0])
+#     print(c,lum,std,min,max,nb0px)
+#     c1.show()
+#
+#
+
+ie = ImageEnhance.Contrast(norm)
+c10 = ie.enhance(10)
+stat = ImageStat.Stat(c10)
+lum = stat.mean[0]
+std = stat.stddev[0]
+min2 = stat.extrema[0][0]
+max2 = stat.extrema[0][1]
+pxs = list(c10.getdata())
+nb0px = len([p for p in pxs if p == 0]) #4:458 5:9403 6:33295
+print(lum, std, min2, max2, nb0px)
+#c10.show()
+
+radius = 400
+im2 =  im.crop(((im.size[0] / 2) - radius, (im.size[1] / 2) - radius, (im.size[0] / 2) + radius, (im.size[1] / 2) + radius))
+im2 = im2.resize((100,100), Image.LANCZOS) # super
+#im2 = im2.resize((500,500), Image.NEAREST)
+ie = ImageEnhance.Contrast(im2)
+im2 = ie.enhance(40)
+pxs = list(im2.getdata())
+nb0px = len([p for p in pxs if p == 0])
+#im2 = im2.point(lambda x : 255 if x == 0 else x)
+print(nb0px)
+#im2.show() # resulat pas terrible
+
+imload = im2.load()
+for x in range(1,99):
+    for y in range(1,99):
+        if imload[x,y] == 0:
+            # imload[x,y] = min(imload[x - 1, y],
+            #                    imload[x + 1, y],
+            #                    imload[x - 1, y - 1],
+            #                    imload[x + 1, y + 1],
+            #                    imload[x - 1, y + 1],
+            #                    imload[x + 1, y - 1],
+            #                    imload[x, y - 1],
+            #                    imload[x, y + 1]
+            #                    )
+            imload[x,y] = min(imload[x + 1, y],
+                               imload[x + 2, y],
+                               imload[x + 3, y],
+                               imload[x + 4, y]
+                               )
+            if imload[x, y] !=0:
+                imload[x, y] = 255
+pxs = list(im2.getdata())
+nb0px = len([p for p in pxs if p == 0])
+print(nb0px)
+#im2.show()
+
+stdmean = 11.65
+stsstd = 2.17
+stdmax = 21.42
+print(127 / stdmean, 127/stdmax)
+stdtestboucle = 4
+# Tester avec 4, 6 et 10
---
+++ wf04-norm-contrast-reduce.py
@@ -0,0 +1,66 @@
+import config
+import json
+import numpy as np
+from PIL import Image, ImageStat, ImageEnhance
+
+print("Data worflow step 4: Normalize, contrast & reduce")
+print("=================================================")
+path = f"{config.path}-small/03-split"
+path = f"{config.path}/03-split"
+outpath = f"{config.path}-small/04-norm"
+outpath = f"{config.path}/04-norm"
+
+def transform(s="train", contrast = 1, size = 224, method = Image.LANCZOS):
+    print(f"Open {s}/db.json")
+    db = None
+    with open(f"{path}/{s}/db.json", "r") as f:
+        db = json.loads(f.read())
+    print(db)
+    db["name"] = f"04-norm-c{contrast}-{size}-{s}"
+    db["path"] = f"{outpath}/c{contrast}-{size}/{s}"
+    db["size"] = size
+    for die in db["dies"]:
+        im = Image.open(die["path"])
+        stat = ImageStat.Stat(im)
+        lum = stat.mean[0]
+        norm = im.point(lambda x: np.clip(x - lum + 127, 0, 255))
+        if contrast != 1:
+            ie = ImageEnhance.Contrast(norm)
+            norm = ie.enhance(contrast)
+        stat = ImageStat.Stat(norm)
+        die["lum"] = round(stat.mean[0],2)
+        die["std"] = round(stat.stddev[0],2)
+        die["lummin"] = stat.extrema[0][0]
+        die["lummax"] = stat.extrema[0][1]
+        red = norm.resize((size,size),method)
+        file = f"{db['path']}/{die['die']}-norm-c{contrast}-{size}.bmp"
+        print(f"Creating {file}")
+        red.save(file)
+
+        radius = 400
+        im0 = im.crop(((im.size[0] / 2) - radius, (im.size[1] / 2) - radius, (im.size[0] / 2) + radius, (im.size[1] / 2) + radius))
+        im0 = im0.resize((100, 100), Image.LANCZOS)
+        ie = ImageEnhance.Contrast(im0)
+        im0 = ie.enhance(40)
+        pxs = list(im0.getdata())
+        nb0px = len([p for p in pxs if p == 0])
+        die["zpc40"] = nb0px
+
+    db["lum"] = round(sum([d["lum"] for d in db["dies"]]) / len(db["dies"]), 2)
+    db["std"] = round(sum([d["std"] for d in db["dies"]]) / len(db["dies"]), 2)
+    db["lummin"] = round(sum([d["lummin"] for d in db["dies"]]) / len(db["dies"]), 2)
+    db["lummax"] = round(sum([d["lummax"] for d in db["dies"]]) / len(db["dies"]), 2)
+    db["zpc40"] = int(sum([d["zpc40"] for d in db["dies"]]) / len(db["dies"]))
+
+    print(f"Create {db['path']}/db.json")
+    with open(f"{db['path']}/db.json", "w") as f:
+        f.write(json.dumps(db, indent=4))
+
+transform("test",1,224)
+transform("train",1,224)
+transform("test",4,224)
+transform("train",4,224)
+transform("test",6,224)
+transform("train",6,224)
+transform("test",10,224)
+transform("train",10,224)
